8565642b114e541eab2557382432060563283242
Alan Bastida <alanbc60@gmail.com>
2022-10-24 19:45:06 -0500

    prueba de colaboracion-cambio de branch

--- hello.rs
+++ hello.rs
@@ -2,5 +2,5 @@ fn main() {
     // Statements here are executed when the compiled binary is called
 
     // Print text to the console
-    println!("Hello World!");
+    println!("Hello World!, modificacion por alanbc");
 }
